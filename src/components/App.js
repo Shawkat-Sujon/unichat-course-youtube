e7a03b88861af3d4a78070d545d224ac3494b4d6
Shawkat-Sujon <sujoniutswe@gmail.com>
2023-06-04 00:53:03 +0600

    Environment Setup done

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div style={{ fontFamily: 'Avenir' }}>
+    <div style={{ fontFamily: 'Avenir' }}>hello mamam
       <Router>
         {/* <AuthProvider> */}
           <Switch>
